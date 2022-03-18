de3764d101c4308e3117639717701f9569eb63db
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-03-18 13:50:42 +0900

    Create self-introduction.rb

---
+++ self-introduction.rb
@@ -0,0 +1,10 @@
+eval(%w(
+    
+puts("Jota        ro-Su
+    gi          ya     ma
+    !t         wi       tt
+    er         :@       su
+gi  jo          ta     ro
+ ,I_l      ov     e_🐈")
+
+)*"")
