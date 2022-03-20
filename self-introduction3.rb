b10289827517e3aaf89ed4bb5ae2e3aad907a384
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-03-20 15:23:00 +0900

    Update self-introduction3.rb

--- self-introduction3.rb
+++ self-introduction3.rb
@@ -140,5 +140,21 @@ W"          +3
  2.        ch
 r+          ""
 
-+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"\n"+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"KK"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"KK"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"\n"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"\n"+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K")).join)
+
+
+
+    +3    2.chr+"W"+32      .c      hr+"W"+3        2.        ch                r+""+32.chr+""
++32.chr+""    +3    2.    ch        r+    ""        +3        2.                      ch
+    r+      "W      "+  32.c  hr  +"    "+      32.chr+""+32.chr+""+32  .chr+"K"      +3
+    2.    ch        r+      ""    +32.chr+"K"+      32        .c            hr      +"
+"+32.chr+""+    32.c      hr  +"  "+  32    .c      hr  +""+32.chr+"        "+    32.chr+"W"+3
+    2.      chr+""+32.  chr+""  +32.  ch    r+      ""+3  2.      ch        r+              ""
++3  2.chr+  "W      "+      32    .chr+"W"+32.  chr+""    +3    2.          ch              r+
+""  +3      2.      ch  r+  ""+3  2.        ch      r+      ""+3            2.ch            r+
+"W"+32      .chr+""+32  .c  hr  +""+                32        .c        hr+"                "+
+32  .c                  hr  +"    K"        +3      2.      ch  r+                        ""
++3    2.chr+"K"+32.chr      +"      "+32.chr+"    "+32  .chr      +""+                32.c
+
+
+hr+""+32.chr+"\n"+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"KK"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"KK"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"\n"+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"\n"+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"W"+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+""+32.chr+"K"+32.chr+""+32.chr+""+32.chr+""+32.chr+"K")).join)
 
